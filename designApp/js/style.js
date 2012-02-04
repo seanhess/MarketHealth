84350a634a3ada0b833c48def6d9e7b68123e692
Sean Clark Hess <seanhess@gmail.com>
2012-02-04 14:25:37 -0700

    design

--- designApp/js/style.js
+++ designApp/js/style.js
@@ -1,4 +1,36 @@
 $(function() {
     var $btn = $('button.add')
     console.log($btn)
+
+
+    // really, I want to wait until certain things are loaded instead
+
+    var itemsToLoad = $(".animateLoad").toArray()
+
+    function animateNext() {
+        var current = itemsToLoad.shift()
+        if (!current) return
+        $(current).addClass('loaded')
+        $(current).cssTransitionEnd(function() {
+            animateNext()
+        })
+    }
+
+
+    animateNext()
 })
+
+
+jQuery.fn.cssTransitionEnd = function(cb) {
+
+    var current = this.get(0)
+
+    current.addEventListener( 'webkitTransitionEnd', onTransitionEnd, false );
+    current.addEventListener( 'transitionend', onTransitionEnd, false );
+    current.addEventListener( 'OTransitionEnd', onTransitionEnd, false );
+
+    function onTransitionEnd() {
+        if (cb) cb()
+        cb = null
+    }
+}
